263a6e2485edaac96e47e778ecd103a62ad73e7b
GCodeON <gcodeondev@gmail.com>
2023-12-08 12:18:24 -0800

    route handler post user

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,11 +1,31 @@
 import prisma from '../../prisma/prisma';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(req: Request, res: Response) {
+export async function GET(req: Request) {
 
     try {
         const users = await prisma.user.findMany()
   
         return Response.json(users)
+  
+    } catch (err) {
+        return Response.json(err)
+    }
+}
+
+export async function POST(req: NextRequest) {
+
+    const formData = await req.json()
+    console.log('for data', formData);
+
+    try {
+        const user = await prisma.user.create({
+            data: formData
+        })
+      
+        if(user) {
+            return Response.json(user)
+        }
   
     } catch (err) {
         return Response.json(err)
