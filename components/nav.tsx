f361400c7fa91e5415d5c9d42a34f17dab8f70b2
GCodeON <gcodeondev@gmail.com>
2023-12-11 11:18:43 -0800

    navbar UI + styles

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,21 +1,36 @@
 'use client'
 import Link from 'next/link'
 import Image from 'next/image'
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Button} from "@nextui-org/react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, Button} from "@nextui-org/react";
 import {NextUIProvider} from '@nextui-org/react'
 
 export function Nav() {
   return (
-    <Navbar shouldHideOnScroll>
-      <NavbarBrand>
-        <Image
-          src="/driscolls.svg"
-          width={100}
-          height={100}
-          alt="logo"
-        />
-      </NavbarBrand>
+    <Navbar isBordered>
+      <NavbarContent className="sm:hidden" justify="start">
+        <NavbarMenuToggle />
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden pr-3" justify="center">
+        <NavbarBrand>
+          <Image
+            src="/driscolls.svg"
+            width={100}
+            height={100}
+            alt="logo"
+          />
+        </NavbarBrand>
+      </NavbarContent>
+
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarBrand>
+          <Image
+            src="/driscolls.svg"
+            width={100}
+            height={100}
+            alt="logo"
+          />
+        </NavbarBrand>
         <NavbarItem>
           <Link  href="/users">
             Users
@@ -26,22 +41,27 @@ export function Nav() {
             News
           </Link>
         </NavbarItem>
-        {/* <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem> */}
+
       </NavbarContent>
-      {/* <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
+
+      <NavbarContent justify="end">
+      </NavbarContent>
+
+      <NavbarMenu>
+        <NavbarItem>
+          <Link  href="/users">
+            Users
+          </Link>
         </NavbarItem>
         <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
+          <Link href="/news">
+            News
+          </Link>
         </NavbarItem>
-      </NavbarContent> */}
+      </NavbarMenu>
     </Navbar>
+
+
+
   )
 }
