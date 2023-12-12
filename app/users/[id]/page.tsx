e6627f87517ea1fecbe6e9a36d706dd81e7c04ba
GCodeON <gcodeondev@gmail.com>
2023-12-12 10:40:20 -0800

    ui styles update

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -3,7 +3,7 @@ import UserForm from '@/components/UserForm';
 
 export default function Page({ params }: { params: { id: string }}) {
   return (
-    <div className="h-screen flex items-center justify-center flex-col gap-4">
+    <div className="h-screen flex max-w-screen-sm m-auto justify-start flex-col gap-4 p-5">
       <h1 className='text-2xl uppercase font-bold text-center p-6'>User Details</h1>
       <UserForm id={params.id}></UserForm>
     </div>
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,7 +2,7 @@ import Table from '@/components/table';
 
 export default async function Page() {
   return (
-    <main className="h-screen flex items-center justify-center flex-col gap-4">
+    <main className="h-screen flex max-w-screen-lg m-auto justify-start flex-col gap-4 p-5">
       <div className='Users'>
         <Table></Table> 
       </div>
